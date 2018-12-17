4cb306894b9ee90d4e587f530e33af7cc2347d97
Yatin Singla <yatin.singla001@gmail.com>
2018-12-17 17:21:52 -0600

    adding the template driven form approach and validation

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -9,17 +10,20 @@ import { ShoppingListService } from '../shopping-list.service';
 })
 export class ShoppingEditComponent implements OnInit {
 
-  @ViewChild('nameInput') nameInput: ElementRef;
-  @ViewChild('amountInput') amountInput: ElementRef;
-
+  // @ViewChild('nameInput') nameInput: ElementRef;
+  // @ViewChild('amountInput') amountInput: ElementRef;
+  @ViewChild('f') shoppingEditForm: NgForm;
+  
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
   onAddItem() {
-    const ingName = this.nameInput.nativeElement.value;
-    const ingAmount = this.amountInput.nativeElement.value;
+    // const ingName = this.nameInput.nativeElement.value;
+    // const ingAmount = this.amountInput.nativeElement.value;
+    const ingName = this.shoppingEditForm.value.name;
+    const ingAmount = this.shoppingEditForm.value.amount;
     const newIngreddient = new Ingredient(ingName, ingAmount);
     this.shoppingListService.addIngredient(newIngreddient);
   }
